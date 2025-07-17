18558c47398ae3ce4ceee2a43b9080a5bbf57ef4
KaduGiffoni <99509537+KaduGiffoni@users.noreply.github.com>
2025-07-17 19:17:30 -0300

    Configure cypress-split

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,15 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require("cypress")
+
+const cypressSplit = require("cypress-split")
 
 module.exports = defineConfig({
   viewportHeight: 1024,
   viewportWidth: 1700,
   e2e: {
+    fixturesFolder: false,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-      fixturesFolder = false;
-    },
+      cypressSplit(on, config)
+      return config
+    }
   },
-  scripts: {},
-});
+})
